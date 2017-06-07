cd0b90580b2231f18921e8e437524003ad20ebf6
pinduzera <ehellas@gmail.com>
2017-06-06 23:31:10 -0300

    0.23
    added optim_mle2, now has bounds

--- R/optim_genlogis2.R
+++ R/optim_genlogis2.R
@@ -3,17 +3,22 @@
 #' Maximum likehood estimation of parameters for a generalized logistic distribution.
 #' 
 #' @param parameters Initial values for the parameters to be optimized over in the following order \code{c(a, b, p, location)},
-#'  \code{location} can be omitted and will be equaled to 0.
+#'  \code{location} can be omioped and will be equaled to 0.
 #' @param data This is the the data to be utilized for the estimation
-#' @keywords d, p, q, r genlogis
+#' @param alpha type 1 error parameter for confidente interval
+#' @keywords d, p, q, r, genlogis
 #' 
 #' @export
 #' @examples 
 #' datas <- rgenlog(10000, 1.5,2,2, 0)
-#' genlog_mle(c(.5,1.6, 1.5, 0),datas)
+#' genlog_mle2(c(.5,1.6, 1.5, 0),datas, alpha = 0.05)
 #' 
 #' @usage 
-#' genlog_mle(parameters, data)
+#' genlog_mle2(parameters, data, alpha = 0.05)
+#' 
+#' @return 
+#' Same as \code{optim} with extra:
+#' \code{bounds} Confidence interval for maximum likelihood parameter estimatives with of 1-\code(alpha) confidence 
 #' 
 #' @details 
 #' Maximum likehood estimation of parameters for the distribution proposed in this package.\cr
@@ -26,15 +31,22 @@
 #' \code{help(dgenlog)} for parameters restrictions.\cr
 
 
-
-genlog_mle <- function(parameters, data){
-
-
-op <- optim(par=parameters, fn = genlogis.loglikehood, x=data,
-      lower = c(1e-10,1e-10,1e-10,-Inf), upper = c(Inf,Inf,Inf,Inf),
-      method = 'L-BFGS-B') 
-
-return(op)
-
+genlog_mle2 <- function(parameters, data, alpha = 0.05){
+  
+  
+  op <- optim(par=parameters, fn = genlogis.loglikehood, x=data,
+              lower = c(0.01,0.01,0.01,-Inf), upper = c(Inf,Inf,Inf,Inf),
+              method = 'L-BFGS-B', hessian = T) 
+  
+  fisher_info <- solve(op$hessian)
+  var_matrix <- diag(fisher_info)
+  var_matrix <- sqrt(var_matrix)
+  upper <- op$par + qnorm(1-alpha) * var_matrix
+  lower <- op$par - qnorm(1-alpha) * var_matrix
+  interval <- data.frame(value=op$par, lower=lower, upper=upper)
+  op$bounds <-  as.matrix(interval)
+  
+  return(op)
+  
 }
 
